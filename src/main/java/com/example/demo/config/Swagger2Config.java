dbd8e582c81e8c3cffbe84cd44335ea2c702ac89
丁新东 <dingxindong2013@163.com>
2020-04-05 16:23:12 +0800

    第一次提交 增加swagger

---
+++ src/main/java/com/example/demo/config/Swagger2Config.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket webApiConfig() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+
+    }
+}
