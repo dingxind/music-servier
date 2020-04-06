459935ff57b8fa0c04c1e4e276afc9211842bbe9
丁新东 <dingxindong2013@163.com>
2020-04-06 17:03:02 +0800

    修改swagger配置

--- src/main/java/com/example/demo/config/Swagger2Config.java
+++ src/main/java/com/example/demo/config/Swagger2Config.java
@@ -1,9 +1,9 @@
 package com.example.demo.config;
 
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,10 +15,17 @@ public class Swagger2Config {
     public Docket webApiConfig() {
 
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .groupName("webApi")
                 .select()
-                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
-                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("轻云出悦") //接口管理文档首页显示
+                .description("轻云出悦 - Swagger使用演示")//API的描述
+                .version("1.0")
                 .build();
     }
 }
